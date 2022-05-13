4bfdb843d3372079355901764108ace5f10e3510
MEddy-in-COM9 <102485991+MEddy-in-COM9@users.noreply.github.com>
2022-05-13 16:15:59 +0200

    Delete video1.py

--- video1.py
+++
@@ -1,9 +0,0 @@
-import machine
-import utime
-
-led = machine.Pin(15, machine.Pin.OUT)
-bottone = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
-
-while True:
-    lled = bottone.value()
-    led.value(lled)
